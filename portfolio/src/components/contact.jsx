e2a43afe62eb8367f9644f7aa7ea23ffffe1aa27
Raja-Sankar-21 <rajasankar2114@gmail.com>
2025-08-06 12:14:13 +0530

    First Commit

--- portfolio/src/components/contact.jsx
+++ portfolio/src/components/contact.jsx
@@ -1,6 +1,6 @@
 const Contact = () => (
   <footer className="contact">
-    <h2>Get in Touch</h2>
+    <h2 style={{color:"white"}}>Get in Touch</h2>
     <p>Email: <a href="mailto:rajasankar2114@gmail.com" target="blank">rajasankar2114@gmail.com</a></p>
     <p>Phone: <a href="tel:+919047886001" target="blank">+91 90478 86001</a></p>
   </footer>
